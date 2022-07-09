d750b43bb97aea146a86c21940914d38c7976304
uzzal71 <uzzalroy.acm@gmail.com>
2022-07-09 19:57:26 +0600

    Nesting links with React Router v6

--- React.js Essential Training/Ch07/07_04/start/react-app/src/App.js
+++ React.js Essential Training/Ch07/07_04/start/react-app/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 function Home() {
   return (
@@ -22,6 +22,15 @@ export function About() {
         <Link to="/contact">Contact</Link>
       </nav>
       <h1>About Us</h1>
+      <Outlet />
+    </div>
+  );
+}
+
+export function History() {
+  return (
+    <div>
+      <h1>Our History</h1>
     </div>
   );
 }
--- React.js Essential Training/Ch07/07_04/start/react-app/src/index.js
+++ React.js Essential Training/Ch07/07_04/start/react-app/src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { App, About, Contact } from "./App";
+import {
+  App,
+  About,
+  Contact,
+  History
+} from "./App";
 import {
   BrowserRouter,
   Routes,
@@ -12,7 +17,12 @@ ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/about" element={<About />} />
+      <Route path="/about" element={<About />}>
+        <Route
+          path="history"
+          element={<History />}
+        />
+      </Route>
       <Route
         path="/contact"
         element={<Contact />}
