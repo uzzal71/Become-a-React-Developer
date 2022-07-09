09e0468fe34d138c955aebf6f1c4f62d34b53484
uzzal71 <uzzalroy.acm@gmail.com>
2022-07-09 18:45:04 +0600

    Displaying data from an API

--- React.js Essential Training/Ch06/06_02/start/react-app/src/App.js
+++ React.js Essential Training/Ch06/06_02/start/react-app/src/App.js
@@ -1,18 +1,34 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 
+function GithubUser({ name, location, avatar, bio }) {
+  return (
+    <div>
+        <h1>{name}</h1>
+        <p>{location}</p>
+        <img src={avatar} height={150} alt={name}/>
+        <p>{bio}</p>
+    </div>
+  );
+}
+
 function App() {
   const [data, setData] = useState(null);
   useEffect(() => {
     fetch(
-      `https://api.github.com/users/moonhighway`
+      `https://api.github.com/users/uzzal71`
     )
       .then((response) => response.json())
-      .then(setData);
+      .then(setData); 
   }, []);
   if (data)
     return (
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <GithubUser 
+        name={data.name}
+        location={data.location}
+        avatar={data.avatar_url}
+        bio={data.bio}
+      />
     );
   return <h1>Data</h1>;
 }
